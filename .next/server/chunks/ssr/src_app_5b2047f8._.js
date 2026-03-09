1d51e1b09e34dd9c6faaca1188765a1b01c8119d
Dode2203 <randriadode@gmail.com>
2026-03-09 22:59:24 +0300

    dede

---
+++ .next/server/chunks/ssr/src_app_5b2047f8._.js
@@ -0,0 +1,3 @@
+module.exports=[10585,a=>{a.v("/_next/static/media/favicon.d2a34508.ico")},68611,a=>{"use strict";let b={src:a.i(10585).default,width:925,height:592};a.s(["default",0,b])}];
+
+//# sourceMappingURL=src_app_5b2047f8._.js.map
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mWZ5RwB30WV9YBMkLgLkqDWoYkueIup84lJkr7MIoTI=\"\n}"
